941fd1ebc13ac8c5140355a0666a9cedb6d278ab
bazzo03 <dfbb1990>
2017-09-30 18:52:38 -0500

    disable logo in the console output when starting - set up a username and password in the application.proerties

--- src/main/java/com/library/security/SecurityConfig.java
+++ src/main/java/com/library/security/SecurityConfig.java
@@ -1,12 +1,10 @@
 //package com.library.security;
 //
+//import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
 //
 //import com.library.repository.UserRepository;
 //
@@ -15,19 +13,17 @@
 //	
 //	private UserRepository userRepository;
 //
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http.authorizeRequests().antMatchers("/").access("hasRole('USER')").antMatchers("/**").authenticated();
-//	}
+//	 @Override
+//	    protected void configure(HttpSecurity http) throws Exception {
+//	        http
+//	                .authorizeRequests()
+//	                .antMatchers("/").fullyAuthenticated();
+//	    } 
 //
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		
-//		auth.userDetailsService(new UserDetailsService() {
-//			@Override
-//			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//				return null;//userRepository.findOne(username);
-//			}
-//		});
-//	}
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//            .inMemoryAuthentication()
+//                .withUser("user").password("password").roles("USER");
+//    }
 //}
