8135130dcb2674a4fe98baa4342ddc5542495bdf
Daniel Fernando Bernal Bazzani <dbernal@dbernal.belatrix.com>
2019-02-06 11:51:48 -0500

    Adding maven dependencies to make JWT compatible with newer Java versions >9

--- src/main/java/com/library/security/WebSecurityConfig.java
+++ src/main/java/com/library/security/WebSecurityConfig.java
@@ -13,13 +13,17 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers("/").permitAll().antMatchers(HttpMethod.POST, "/login")
-				.permitAll().anyRequest().authenticated().and()
-				// We filter the api/login requests
-				.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
-						UsernamePasswordAuthenticationFilter.class)
-				// And filter other requests to check the presence of JWT in header
-				.addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.csrf().disable();
+
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers(HttpMethod.POST, "/login").permitAll()
+				.anyRequest().authenticated();
+
+		// We filter the api/login requests
+		http.addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
+				UsernamePasswordAuthenticationFilter.class);
+
+		// And filter other requests to check the presence of JWT in header
+		http.addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
 
 	@Override
